2e2e8b30993128630fc67c629b846731a342a03a
fiorentinogiuseppe <fiorentinogiuseppebcc@gmail.com>
2020-03-25 11:24:10 -0300

    Nova aula 4

--- main.cpp
+++ main.cpp
@@ -74,6 +74,7 @@ int vid3(){
     if(glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
+    std::cout << glGetString(GL_VERSION) << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -105,6 +106,60 @@ int vid3(){
     return 0;
 }
 
+
+// Vertex Buffers and Drawing a Triangle in OpenGL
+int vid4(){
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+            -0.5f, -0.5f,
+             0.0f,  0.5f,
+             0.5f, -0.5f,
+    };
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
 int main(){
-    return vid3();
+    return vid4();
 }
