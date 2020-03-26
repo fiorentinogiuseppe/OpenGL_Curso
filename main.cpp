ce0b16b0da7140aadc9e3b5786ec32630121a765
fiorentinogiuseppe <fiorentinogiuseppebcc@gmail.com>
2020-03-26 10:59:54 -0300

    Aula 5 e 6

--- main.cpp
+++ main.cpp
@@ -131,8 +131,9 @@ int vid4(){
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    // X, Y
     float positions[6] = {
-            -0.5f, -0.5f,
+            -0.5f, -0.5f, // 2 floats per vertex
              0.0f,  0.5f,
              0.5f, -0.5f,
     };
@@ -160,6 +161,71 @@ int vid4(){
     return 0;
 }
 
+// Vertex Attributes and Layouts in OpenGL
+int vid5(){
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if(glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+            -0.5f, -0.5f,
+            0.0f,  0.5f,
+            0.5f, -0.5f,
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLAT, GL_FALSE, sizeof(float) * 2, (const void*) 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+//How Shaders Work in OpenGL
+void vid6(){
+    std::cout << "How Shaders Work in OpenGL" << std::endl;
+
+}
+
 int main(){
-    return vid4();
+    return vid5();
 }
